de8663663c8b431504fceb44f416636d31208d5d
Shrey Pasricha <shrey.pasricha@gmail.com>
2019-07-09 13:00:59 -0400

    Implemented K-Fold cross validation.

--- main.py
+++ main.py
@@ -1,28 +1,37 @@
-import math
-import random
-from collections import defaultdict
-from pprint import pprint
-
-import seaborn as sns
-import matplotlib.pyplot as plt
-import numpy as np
 import pandas as pd
-
-from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.model_selection import train_test_split
-from sklearn.naive_bayes import MultinomialNB
-from sklearn.metrics import accuracy_score, f1_score, confusion_matrix
-
+import seaborn as sns
 from imblearn.over_sampling import SMOTE
-
-sm = SMOTE()
-nb = MultinomialNB()
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.metrics import accuracy_score, f1_score, confusion_matrix
+from sklearn.model_selection import train_test_split, ShuffleSplit
+from sklearn.naive_bayes import MultinomialNB
 
 # Set global styles for plots
 sns.set_style(style='white')
 sns.set_context(context='notebook', font_scale=1.3, rc={'figure.figsize': (16, 9)})
 
-df = pd.read_csv('datagt/glassdoor_v3.csv', nrows=50000)
+ss = ShuffleSplit(n_splits=10, test_size=0.2)
+vect = CountVectorizer(max_features=1000, binary=True)
+sm = SMOTE()
+nb = MultinomialNB()
+
+accs = []
+f1s = []
+cms = []
+
+
+def predict_sentiment(text):
+    pred = nb.predict(vect.transform([text]))
+    return pred[0]
+
+
+def print_accuracy():
+    print("\nAverage accuracy across folds: {:.2f}%".format(sum(accs) / len(accs) * 100))
+    print("\nAverage F1 score across folds: {:.2f}%".format(sum(f1s) / len(f1s) * 100))
+    print("\nAverage Confusion Matrix across folds: \n {}".format(sum(cms) / len(cms)))
+
+
+df = pd.read_csv('datagt/glassdoor_v3.csv', nrows=1000)
 
 positive_pros = df.loc[df.Rating_overall.isin([4, 5])].Pros.to_frame(name='text')
 positive_pros['sentiment'] = 1
@@ -34,20 +43,31 @@ negative_cons = df.loc[df.Rating_overall.isin([1, 2])].Cons.to_frame(name='text'
 negative_cons['sentiment'] = -1
 
 data = pd.concat([positive_pros, neutral_pros, neutral_cons, negative_cons])
+data.reset_index(drop=True, inplace=True)
 
-text_train, text_test, sentiment_train, sentiment_test = train_test_split(data.text, data.sentiment, test_size=0.2)
+current_fold = 1
 
-vect = CountVectorizer(max_features=1000, binary=True)
-text_train_vect = vect.fit_transform(text_train)
+for train_index, test_index in ss.split(data.text):
+    print(f"On fold: {current_fold}")
+    current_fold += 1
+    text_train, text_test = data.text.iloc[train_index], data.text.iloc[test_index]
+    sentiment_train, sentiment_test = data.sentiment.iloc[train_index], data.sentiment.iloc[test_index]
 
-text_train_res, sentiment_train_res = sm.fit_sample(text_train_vect, sentiment_train)
+    # Fit vectorizer and transform text train, then transform text test
+    text_train_vect = vect.fit_transform(text_train)
+    text_test_vect = vect.transform(text_test)
 
-nb.fit(text_train_res, sentiment_train_res)
-nb.score(text_train_res, sentiment_train_res)
-text_test_vect = vect.transform(text_test)
-sentiment_pred = nb.predict(text_test_vect)
+    # Oversample
+    text_train_res, sentiment_train_res = sm.fit_sample(text_train_vect, sentiment_train)
 
-print("Accuracy: {:.2f}%".format(accuracy_score(sentiment_test, sentiment_pred) * 100))
-print("\nF1 Score: {:.2f}".format(f1_score(sentiment_test, sentiment_pred) * 100))
-print("\nCOnfusion Matrix:\n", confusion_matrix(sentiment_test, sentiment_pred))
+    # Fit Naive Bayes on the vectorized text with sentiment train labels,
+    # then predict new sentiment labels using text test
+    nb.fit(text_train_res, sentiment_train_res)
+    sentiment_pred = nb.predict(text_test_vect)
 
+    # Determine test set accuracy and f1 score on this fold using the true sentiment labels and predicted sentiment labels
+    accs.append(accuracy_score(sentiment_test, sentiment_pred))
+    f1s.append(f1_score(sentiment_test, sentiment_pred))
+    cms.append(confusion_matrix(sentiment_test, sentiment_pred))
+
+print_accuracy()
