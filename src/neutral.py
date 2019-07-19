3b28e9a299cbba4008f65191fbf02fd0451650a9
Shrey Pasricha <shrey.pasricha@gmail.com>
2019-07-19 00:00:08 -0400

    Created show features function to show feature counts.

--- src/neutral.py
+++ src/neutral.py
@@ -1,4 +1,5 @@
 import collections
+import re
 
 import matplotlib.pyplot as plt
 import numpy as np
@@ -38,6 +39,28 @@ def word_frequency(clean_text):
 	plt.show()
 
 
+def show_features(estimator, class_names=None):
+	# TODO: Use transformer names or 'final estimator' to make this more decoupled.
+	if class_names is None:
+		class_names = ['Negative', 'Neutral', 'Positive']
+	best_mk_ct = estimator.best_estimator_.steps[0][1].mk_ct
+	best_clf = estimator.best_estimator_.steps[2][1]
+
+	feature_names = [re.sub('^.{0,16}', '', name) for name in best_mk_ct.get_feature_names()]
+	feature_count = best_clf.feature_count_
+	features_df = pd.DataFrame(list(zip(feature_names, *feature_count)), columns=['Name', *class_names])
+	#features_df = features_df.sort_values('Positive')
+	#sns.heatmap(features_df.drop('Name', axis=1)[-10:], cbar=False, yticklabels=features_df.Name[-10:])
+	#plt.show()
+
+	for clazz in class_names:
+		fig, (ax1, ax2) = plt.subplots(2, 1, sharex=True, figsize=(16, 9))
+		class_features = features_df.sort_values(clazz)[::-1]
+		plt.figure(figsize=(12, 5))
+		sns.barplot(x=class_features.Name[:10], y=class_features[clazz][:10])
+		plt.show()
+
+
 np.random.seed(37)
 text_cols = ['Review_starttext', 'Pros', 'Cons']
 y_axis = 'Rating_overall'
@@ -47,7 +70,7 @@ df_data = df[[*text_cols, y_axis]]  # Remove unused data
 df_data = df_data.dropna()  # Remove rows with empty text
 df_data = df_data.reindex(np.random.permutation(df_data.index))
 df_data[y_axis] = df_data[y_axis].replace([1, 2, 3, 4, 5], [-1, -1, 0, 1, 1])  # Replace ratings with classifier classes
-df_data = df_data[:20000]  # Limit to first N entries, only for quick testing.
+df_data = df_data[:100000]  # Limit to first N entries, only for quick testing.
 
 # Preprocessing, this shouldn't be done in the grid search
 preprocess = make_column_transformer((CleanText(), text_cols), remainder='passthrough')
@@ -55,11 +78,11 @@ df_processed = preprocess.fit_transform(df_data)
 
 # Column transformer converts the data to an ndarray, so this converts it back to a dataframe
 df_processed = pd.DataFrame(data=df_processed,  # values
-                            index=df_data.index,  # 1st column as index
-                            columns=df_data.columns)  # 1st row as the column names
+							index=df_data.index,  # 1st column as index
+							columns=df_data.columns)  # 1st row as the column names
 
 x_train, x_test, y_train, y_test = train_test_split(df_processed.drop(y_axis, axis=1), df_processed[y_axis],
-                                                    test_size=0.2, random_state=37)
+													test_size=0.2, random_state=37)
 
 # Parameter grid settings for the vectorizers (Count and TFIDF)
 parameters_vect = {
@@ -86,7 +109,7 @@ vect = TfidfVectorizer()
 
 # MultinomialNB
 best_mnb_countvect = grid_vect(mnb, parameters_mnb, x_train, x_test, y_train, y_test, text_cols,
-                               parameters_text=parameters_vect, vect=vect)
+							   parameters_text=parameters_vect, vect=vect)
 # joblib.dump(best_mnb_countvect, './output/best_mnb_tfidfvect.pkl')
 # LogisticRegression
 # best_logreg_countvect = grid_vect(logreg, parameters_logreg, x_train, x_test, y_train, y_test, text_cols, parameters_text=parameters_vect, vect=vect)
@@ -95,11 +118,4 @@ best_mnb_countvect = grid_vect(mnb, parameters_mnb, x_train, x_test, y_train, y_
 con_matrix = confusion_matrix(np.asarray(y_test, dtype='int64'), best_mnb_countvect.predict(x_test))
 pretty_plot_confusion_matrix(pd.DataFrame(con_matrix, columns=['negative', 'neutral', 'positive']))
 
-# TODO: Use transformer names or 'final estimator' to make this more decoupled.
-best_mk_ct = best_mnb_countvect.best_estimator_.steps[0][1].mk_ct
-best_clf = best_mnb_countvect.best_estimator_.steps[2][1]
-
-feature_names = best_mk_ct.get_feature_names()
-feature_count = best_clf.feature_count_
-features_df = pd.DataFrame(list(zip(feature_names, *feature_count)),
-                           columns=['Name', 'Negative', 'Neutral', 'Positive'])
+show_features(best_mnb_countvect)
